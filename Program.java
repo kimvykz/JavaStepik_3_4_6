459652f9dd20ecaef1a023af3421b72b4544140d
kimvykz <kimslavakz@gmail.com>
2023-05-12 00:35:20 +0600

    Program created

--- Program.java
+++ Program.java
@@ -1,35 +1,43 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
+import java.awt.event.*;
 
 public class Program {
 
+    static  Point pStart;
     public static void main(String[] args) {
         JFrame frame = new JFrame("Moving object");
         JPanel panel = new JPanel();
-        JLabel label = new JLabel("test");
-
+        JLabel label = new JLabel("Move this object with RButton");
 
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
-        frame.setLayout(new BorderLayout());
+
         panel.setPreferredSize(new Dimension(500, 500));
+        panel.setLayout(new GridBagLayout());
 
-        label.addMouseMotionListener(new MouseMotionListener() {
-            @Override
-            public void mouseDragged(MouseEvent e) {
-                label.setBounds(e.getX(),e.getY(), label.getWidth(), label.getHeight());
-            }
+        label.addMouseListener(new MouseAdapter() {
 
             @Override
-            public void mouseMoved(MouseEvent e) {
-
+            public void mousePressed(MouseEvent e) {
+                pStart = e.getPoint();
             }
         });
 
+        label.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                if (e.getButton() == 3) {
+                    Point pEdt = SwingUtilities.convertPoint(label, e.getPoint(), panel);
+                    label.setLocation(pEdt.x - pStart.x, pEdt.y - pStart.y);
+                }
+            }
+
+
+        });
+
+        panel.setFocusable(true);
         panel.add(label);
         frame.add(panel);
 
